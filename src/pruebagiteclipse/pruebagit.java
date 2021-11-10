3714dad3d49542035f1c3a78316cf3e35bdfa790
Cristian <cristianfv61@educastur.es>
2021-11-10 13:08:20 +0100

    tercer commit

--- src/pruebagiteclipse/pruebagit.java
+++ src/pruebagiteclipse/pruebagit.java
@@ -4,9 +4,10 @@ public class pruebagit {
 
 	public static void main(String[] args) {
 		
-		System.out.println("pribando git...");
+		System.out.println("probando git...");
 		System.out.println("otra prueba...");
-
+		System.out.println("tercera prueba...");
+		System.out.println("otra prueba...");
 	}
 
 }
